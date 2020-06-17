97cc097ec7484d53c1895397e6f292c5fcb8ed07
Chris Chandler <chris@chrischandler.com>
2020-06-17 16:35:18 +0800

    Miscellaneous bug fixes

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
 	};
 
 	onSubmitSignin = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://secure-hollows-71583.herokuapp.com/signin', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
